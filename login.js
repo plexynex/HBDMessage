cd727e95358caa02cb89151fef589b3dedd5a3fb
M Sukri <142651234+Msxky@users.noreply.github.com>
2023-11-10 14:16:31 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,38 @@
+function btnclick() {
+  var audio = document.getElementById("audio");
+  var username = document.getElementById("username").value;
+  let btn = document.getElementById("btn");
+  let label = document.getElementById("label");
+  let box = document.getElementById("box");
+  let video = document.getElementById("video");
+  let a = document.getElementById("a");
+  let text = document.getElementById("text");
+
+  const pass = "123";
+
+  if (username == pass) {
+    audio.play();
+    text.textContent = "✅password benar✅";
+    btn.textContent = "Tunggu bentar..";
+    setTimeout(function () {
+      text.style.fontSize = "12px";
+      text2.textContent =
+        "Hai" + " " + "(nama)" + "..." + " " + "Happy Birthday🥳";
+      text.style.fontFamily = "'Poppins', sans-serif";
+      text.textContent = "isi pesan + pw bisa reques ama yg ngirim link";
+      btn.style.display = "none";
+      label.style.display = "none";
+      box.style.display = "none";
+      video.style.display = "none";
+      a.style.width = "100%";
+      a.style.cursor = "pointer";
+      a.style.height = "100vh";
+      a.style.border = "none";
+      a.style.backdropFilter = "blur(2px)";
+    }, 8500);
+  } else if (username == 0) {
+    text.textContent = "🙁please input password🙁";
+  } else {
+    text.textContent = "🚫password salah🚫";
+  }
+}
