c1f39b9f28b8834e537ab51d607c413d88be2dc7
M Sukri <142651234+plexynex@users.noreply.github.com>
2025-08-02 00:40:14 +0700

    Update login.js

--- login.js
+++ login.js
@@ -12,7 +12,7 @@ function btnclick() {
   let a = document.getElementById("a");
   let text = document.getElementById("text");
 
-  const pass = "0107";
+  const pass = "3107";
   
   if (username == pass) {
     audio.play();
@@ -20,7 +20,7 @@ function btnclick() {
     btn.textContent = "Tunggu bentar..";
     setTimeout(function () {
       text.style.fontSize = "12px";
-      text2.textContent ="Hai" + " " + "someone" + "..." + " " + "Happy Birthday🥳";
+      text2.textContent ="Hai" + " " + "Nezaa" + "..." + " " + "Happy Birthday🥳";
       //text2.textContent = "Happy Birthday🥳";
       text.style.fontFamily = "'Poppins', sans-serif";
       text.textContent = "I hope today is the best for you. nih kado online hihi";
