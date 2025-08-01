31746402aff2563f03cec41002ee572c0c0ae5ee
M Sukri <142651234+plexynex@users.noreply.github.com>
2025-08-02 02:18:50 +0700

    Update login.js

--- login.js
+++ login.js
@@ -12,7 +12,7 @@ function btnclick() {
   let a = document.getElementById("a");
   let text = document.getElementById("text");
 
-  const pass = "3107";
+  const pass = "1234";
   
   if (username == pass) {
     audio.play();
@@ -20,7 +20,7 @@ function btnclick() {
     btn.textContent = "Tunggu bentar..";
     setTimeout(function () {
       text.style.fontSize = "12px";
-      text2.textContent ="Hai" + " " + "Nezaa" + "..." + " " + "Happy Birthday🥳";
+      text2.textContent ="Hai" + " " + "Zaraa" + "..." + " " + "Happy Birthday🥳";
       //text2.textContent = "Happy Birthday🥳";
       text.style.fontFamily = "'Poppins', sans-serif";
       text.textContent = "I hope today is the best for you. nih kado online hihi";
@@ -57,3 +57,4 @@ function triggerBtn() {
     btnAnimate.textContent="PLAY"
   }
 }
+
