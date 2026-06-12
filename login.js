36abd244d2b29df17de5ef887e0eb4fe8243c001
M Sukri <142651234+plexynex@users.noreply.github.com>
2026-06-12 23:04:34 +0700

    Update login.js

--- login.js
+++ login.js
@@ -63,7 +63,49 @@ function triggerBtn() {
   }
 }
 
+// matiin inspect elemen
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+});
 
+document.addEventListener("keydown", function(e) {
+
+    // F12
+    if (e.key === "F12") {
+        e.preventDefault();
+        return false;
+    }
+
+    // Ctrl+Shift+I
+    if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") {
+        e.preventDefault();
+        return false;
+    }
+
+    // Ctrl+Shift+J
+    if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "j") {
+        e.preventDefault();
+        return false;
+    }
+
+    // Ctrl+Shift+C
+    if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "c") {
+        e.preventDefault();
+        return false;
+    }
+
+    // Ctrl+U
+    if (e.ctrlKey && e.key.toLowerCase() === "u") {
+        e.preventDefault();
+        return false;
+    }
+
+    // Ctrl+S
+    if (e.ctrlKey && e.key.toLowerCase() === "s") {
+        e.preventDefault();
+        return false;
+    }
+});
 
 
 
